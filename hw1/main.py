7c761da1761b2d924b45586473174105d30fd83e
andy94077 <andy94077@gmail.com>
2020-03-07 01:00:08 +0800

    Update load_train_data in main.py

--- hw1/main.py
+++ hw1/main.py
@@ -5,13 +5,25 @@ import pandas as pd
 from Adam import Adam
 
 def load_train_data(hr):
-    pass
+    train = pd.read_csv('train.csv', encoding = 'big5').iloc[:, 3:]
+    train[train == 'NR'] = 0
+    train = train.to_numpy().reshape(12, 20, 18, 24).astype(np.float32) # month, day, measurement, time
+    train = np.transpose(train, [0, 2, 1, 3]).reshape(12, 18, -1) # month, measurement, day * time
+    train[:, 9] = np.abs(train[:, 9])
+    trainX = np.concatenate([m[:, i:i+hr].reshape(1, -1) for m in train for i in range(m.shape[1] - hr)], axis=0)
+    mean = np.mean(trainX, axis=0)
+    std = np.std(trainX, axis=0)
+    trainX = np.concatenate([np.ones((trainX.shape[0], 1), np.float32), (trainX - mean) / (std + 1e-10)], axis=1)
+    trainY = train[:, 9, hr:].reshape(-1, 1)
+    return trainX, trainY, mean, std
 
 def load_test_data(path, mean, std):
     pass
 
 def train_test_split(X, Y, split_ratio=0.2):
-    pass
+    np.random.seed(880301)
+    idx = np.random.permutation(X.shape[0])
+    return X[:-int(X.shape[0] * split_ratio)], X[-int(X.shape[0] * split_ratio):], Y[:-int(Y.shape[0] * split_ratio)], Y[-int(Y.shape[0] * split_ratio):]
 
 def rmse(X, Y, w):
     return np.sqrt(np.mean(X @ w - Y) ** 2)

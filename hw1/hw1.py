3901f30f334d790a1d1552db80db84c55fb6f0cb
andy94077 <andy94077@gmail.com>
2020-03-08 00:20:05 +0800

    Add train_file argument and fix typo in hw1.py

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -15,18 +15,20 @@ def compute_gradient(XTX, XTY, w):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('model_path')
+    parser.add_argument('-t', '--training-file')
     parser.add_argument('-T', '--no-training', action='store_true')
     parser.add_argument('-s', '--test', nargs=2, help='testing file and the predicted file')
     parser.add_argument('--lr', type=float, default=1e-3, help='learning rate. (default: %(default)s)')
     args = parser.parse_args()
 
     model_path = args.model_path
+    train_file = args.training_file
     training = not args.no_training
     test = args.test
     lr = args.lr
 
     if training:
-        trainX, trainY, mean, std = utils.load_train_data(9)
+        trainX, trainY, mean, std = utils.load_train_data(train_file, 9)
         np.save('mean.npy', mean)
         np.save('std.npy', std)
 
@@ -34,9 +36,9 @@ if __name__ == '__main__':
 
         XTX = trainX.T @ trainX
         XTY = trainX.T @ trainY
-        epoches = 40000
+        epochs = 40000
         w = np.random.normal(0, 0.05, size=(trainX.shape[1], 1)).astype(np.float32)
-        for epoch in range(epoches):
+        for epoch in range(epochs):
             optimizer.update(XTX, XTY, w)
             if epoch % 100 == 0:
                 print(f'epoch {epoch + 100:04}, loss: {rmse(trainX, trainY, w):.5}')

dabda3161d3a273cf3522555df31f34f69210d32
andy94077 <andy94077@gmail.com>
2020-03-07 21:38:31 +0800

    Remove comments in Adam.py

--- hw1/Adam.py
+++ hw1/Adam.py
@@ -10,7 +10,6 @@ class Adam():
 
 	def update(self, XTX, XTY, w):
 		g = self.gradient_func(XTX, XTY, w)
-		#print(np.linalg.norm(g))
 		self.m = self.beta1 * self.m + (1 - self.beta1) * g
 		self.v = self.beta2 * self.v + (1 - self.beta2) * g ** 2
 		self.beta1_power *= self.beta1
@@ -18,6 +17,5 @@ class Adam():
 		m_hat = self.m / (1 - self.beta1_power)
 		v_hat = self.v / (1 - self.beta2_power)
 		w -= self.eta * m_hat / (np.sqrt(v_hat) + 1e-8)
-		# print(np.linalg.norm(self.m), np.linalg.norm(self.v))
 		
 		

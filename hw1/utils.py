aac5be448d3d323758f708d5a97f2f79d5e2bba0
andy94077 <andy94077@gmail.com>
2020-03-08 23:38:20 +0800

    Organize functions in utils.py

--- hw1/utils.py
+++ hw1/utils.py
@@ -2,12 +2,24 @@ import numpy as np
 import pandas as pd
 import pickle
 
+def data_preprocessing(X):
+    for i in range(X.shape[0]):
+        for j in range(X.shape[-1]):
+            if X[i, 9, j] >= 0:
+                continue
+            if j == 0:
+                X[i, 9, j] = X[i, 9, min(j + 1, X.shape[-1])]
+            elif j == X.shape[-1] - 1:
+                X[i, 9, j] = X[i, 9, max(j - 1, 0)]
+            else:
+                X[i, 9, j] = (X[i, 9, j - 1] + X[i, 9, j + 1]) / 2
+
 def load_train_data(path, hr):
     train = pd.read_csv(path, encoding = 'big5').iloc[:, 3:]
     train[train == 'NR'] = 0
     train = train.to_numpy().reshape(12, 20, 18, 24).astype(np.float32) # month, day, measurement, time
     train = np.transpose(train, [0, 2, 1, 3]).reshape(12, 18, -1) # month, measurement, day * time
-    train[:, 9][train[:, 9] < 0] = 0
+    data_preprocessing(train)
     trainX = np.concatenate([m[:, i:i+hr].reshape(1, -1) for m in train for i in range(m.shape[1] - hr)], axis=0)
     mean = np.mean(trainX, axis=0)
     std = np.std(trainX, axis=0)
@@ -15,53 +27,31 @@ def load_train_data(path, hr):
     trainY = train[:, 9, hr:].reshape(-1, 1)
     return trainX, trainY, mean, std
 
-def load_train_data_sin(path, hr, normalize=True):
+def load_train_data_sin(path, hr):
     train = pd.read_csv(path, encoding = 'big5').iloc[:, 3:]
     train[train == 'NR'] = 0
     train = train.to_numpy().reshape(12, 20, 18, 24).astype(np.float32) # month, day, measurement, time
     train = np.transpose(train, [0, 2, 1, 3]).reshape(12, 18, -1) # month, measurement, day * time
-    train[:, 9][train[:, 9] < 0] = 0
+    data_preprocessing(train)
+
     trainY = train[:, 9, hr:].reshape(-1, 1)
-    train = train[:, [0, 1, 3, 4, 5, 6, 7, 8, 9, 11, 12]]
-    #train[:, 14] = np.sin(train[:, 14] * np.pi / 180)
-    #train[:, 15] = np.sin(train[:, 15] * np.pi / 180)
-    #train = np.concatenate([train, np.cos(train[:, 14:15] * np.pi / 180), np.cos(train[:, 15:16] * np.pi / 180)], axis=1)
+
+    t14, t15 = train[:, 14:15].copy(), train[:, 15:16].copy()
+    train[:, 14] = np.sin(t14[:, 0] * np.pi / 180)
+    train[:, 15] = np.sin(t15[:, 0] * np.pi / 180)
+    train = np.concatenate([train, np.cos(t14 * np.pi / 180), np.cos(t15 * np.pi / 180)], axis=1)
+    
     trainX = np.concatenate([m[:, i:i+hr].reshape(1, -1) for m in train for i in range(m.shape[1] - hr)], axis=0)
     mean = np.mean(trainX, axis=0)
     std = np.std(trainX, axis=0)
-    if normalize:
-        trainX = (trainX - mean) / (std + 1e-10)
-    trainX = np.concatenate([np.ones((trainX.shape[0], 1), np.float32), trainX], axis=1)
+    trainX = np.concatenate([np.ones((trainX.shape[0], 1), np.float32), (trainX - mean) / (std + 1e-10)], axis=1)
     return trainX, trainY, mean, std
-
-def load_train_data_RNN(path, hr):
-    train = pd.read_csv(path, encoding = 'big5').iloc[:, 3:]
-    train[train == 'NR'] = 0
-    train = train.to_numpy().reshape(12, 20, 18, 24).astype(np.float32) # month, day, measurement, time
-    train = np.transpose(train, [0, 1, 3, 2]).reshape(12, -1, 18) # month, day * time, measurement
-    train[:, :, 9][train[:, :, 9] < 0] = 0
-    mean = np.mean(train, axis=(0, 1))
-    std = np.std(train, axis=(0, 1))
-    trainX = np.array([m[i:i+hr] for m in train for i in range(m.shape[0] - hr)])
-    #mean = np.mean(trainX, axis=0)
-    #std = np.std(trainX, axis=0)
-    trainX = (trainX - mean) / (std + 1e-10)
-    trainY = train[:, hr:, 9].ravel()
-    return trainX, trainY, mean, std
-    
-def load_test_data_RNN(path, mean, std):
-    test = pd.read_csv(path, header=None, encoding = 'big5').iloc[:, 2:]
-    test[test == 'NR'] = 0
-    test = test.to_numpy().reshape(-1, 18, 9).transpose([0, 2, 1]).astype(np.float32)
-    test[:, :, 9][test[:, :, 9] < 0] = 0
-    test = (test - mean) / (std + 1e-10)
-    return test
 
 def load_test_data(path, mean, std):
     test = pd.read_csv(path, header=None, encoding = 'big5').iloc[:, 2:]
     test[test == 'NR'] = 0
     test = test.to_numpy().reshape(-1, 18, 9).astype(np.float32)
-    test[:, 9][test[:, 9] < 0] = 0
+    data_preprocessing(test)
     test = test.reshape(-1, 18 * 9)
     test = np.concatenate([np.ones((test.shape[0], 1), np.float32), (test - mean) / (std + 1e-10)], axis=1)
     return test
@@ -70,10 +60,14 @@ def load_test_data_sin(path, mean, std):
     test = pd.read_csv(path, header=None, encoding = 'big5').iloc[:, 2:]
     test[test == 'NR'] = 0
     test = test.to_numpy().reshape(-1, 18, 9).astype(np.float32)
-    test[:, 9][test[:, 9] < 0] = 0
-    test[:, 10] = np.sin(test[:, 15] * np.pi / 180)
-    test[:, 15] = np.cos(test[:, 15] * np.pi / 180)
-    test = test.reshape(-1, 18 * 9)
+    data_preprocessing(test)
+    
+    t14, t15 = test[:, 14:15].copy(), test[:, 15:16].copy()
+    test[:, 14] = np.sin(t14[:, 0] * np.pi / 180)
+    test[:, 15] = np.sin(t15[:, 0] * np.pi / 180)
+    test = np.concatenate([test, np.cos(t14 * np.pi / 180), np.cos(t15 * np.pi / 180)], axis=1)
+    
+    test = test.reshape(-1, test.shape[-2] * 9)
     test = np.concatenate([np.ones((test.shape[0], 1), np.float32), (test - mean) / (std + 1e-10)], axis=1)
     return test
 

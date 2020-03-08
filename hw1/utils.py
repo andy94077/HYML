6ba4698bd520e6ccdfc0c90ae5ff479358764bbd
andy94077 <andy94077@gmail.com>
2020-03-08 21:22:45 +0800

    Add various loading data function in utils.py

--- hw1/utils.py
+++ hw1/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import pickle
 
 def load_train_data(path, hr):
     train = pd.read_csv(path, encoding = 'big5').iloc[:, 3:]
@@ -14,15 +15,36 @@ def load_train_data(path, hr):
     trainY = train[:, 9, hr:].reshape(-1, 1)
     return trainX, trainY, mean, std
 
+def load_train_data_sin(path, hr, normalize=True):
+    train = pd.read_csv(path, encoding = 'big5').iloc[:, 3:]
+    train[train == 'NR'] = 0
+    train = train.to_numpy().reshape(12, 20, 18, 24).astype(np.float32) # month, day, measurement, time
+    train = np.transpose(train, [0, 2, 1, 3]).reshape(12, 18, -1) # month, measurement, day * time
+    train[:, 9][train[:, 9] < 0] = 0
+    trainY = train[:, 9, hr:].reshape(-1, 1)
+    train = train[:, [0, 1, 3, 4, 5, 6, 7, 8, 9, 11, 12]]
+    #train[:, 14] = np.sin(train[:, 14] * np.pi / 180)
+    #train[:, 15] = np.sin(train[:, 15] * np.pi / 180)
+    #train = np.concatenate([train, np.cos(train[:, 14:15] * np.pi / 180), np.cos(train[:, 15:16] * np.pi / 180)], axis=1)
+    trainX = np.concatenate([m[:, i:i+hr].reshape(1, -1) for m in train for i in range(m.shape[1] - hr)], axis=0)
+    mean = np.mean(trainX, axis=0)
+    std = np.std(trainX, axis=0)
+    if normalize:
+        trainX = (trainX - mean) / (std + 1e-10)
+    trainX = np.concatenate([np.ones((trainX.shape[0], 1), np.float32), trainX], axis=1)
+    return trainX, trainY, mean, std
+
 def load_train_data_RNN(path, hr):
     train = pd.read_csv(path, encoding = 'big5').iloc[:, 3:]
     train[train == 'NR'] = 0
     train = train.to_numpy().reshape(12, 20, 18, 24).astype(np.float32) # month, day, measurement, time
     train = np.transpose(train, [0, 1, 3, 2]).reshape(12, -1, 18) # month, day * time, measurement
     train[:, :, 9][train[:, :, 9] < 0] = 0
+    mean = np.mean(train, axis=(0, 1))
+    std = np.std(train, axis=(0, 1))
     trainX = np.array([m[i:i+hr] for m in train for i in range(m.shape[0] - hr)])
-    mean = np.mean(trainX, axis=0)
-    std = np.std(trainX, axis=0)
+    #mean = np.mean(trainX, axis=0)
+    #std = np.std(trainX, axis=0)
     trainX = (trainX - mean) / (std + 1e-10)
     trainY = train[:, hr:, 9].ravel()
     return trainX, trainY, mean, std
@@ -44,7 +66,32 @@ def load_test_data(path, mean, std):
     test = np.concatenate([np.ones((test.shape[0], 1), np.float32), (test - mean) / (std + 1e-10)], axis=1)
     return test
 
+def load_test_data_sin(path, mean, std):
+    test = pd.read_csv(path, header=None, encoding = 'big5').iloc[:, 2:]
+    test[test == 'NR'] = 0
+    test = test.to_numpy().reshape(-1, 18, 9).astype(np.float32)
+    test[:, 9][test[:, 9] < 0] = 0
+    test[:, 10] = np.sin(test[:, 15] * np.pi / 180)
+    test[:, 15] = np.cos(test[:, 15] * np.pi / 180)
+    test = test.reshape(-1, 18 * 9)
+    test = np.concatenate([np.ones((test.shape[0], 1), np.float32), (test - mean) / (std + 1e-10)], axis=1)
+    return test
+
 def train_test_split(X, Y, split_ratio=0.2):
     np.random.seed(880301)
     idx = np.random.permutation(X.shape[0])
     return X[:-int(X.shape[0] * split_ratio)], X[-int(X.shape[0] * split_ratio):], Y[:-int(Y.shape[0] * split_ratio)], Y[-int(Y.shape[0] * split_ratio):]
+
+def load_model(path):
+    with open(path, 'rb') as f:
+        return pickle.load(f)
+
+def save_model(path, model):
+    with open(path, 'wb') as f:
+        pickle.dump(model, f)
+
+def generate_csv(y_pred, output_file):
+    Y = np.clip(np.round(y_pred), 0, np.inf)
+    df = pd.DataFrame(list(zip([f'id_{i}' for i in range(Y.shape[0])], Y.ravel())), columns=['id', 'value'])
+    df.to_csv(output_file, index=False)
+

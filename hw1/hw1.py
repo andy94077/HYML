944c031ebd992a0d0cc4bec338eac4773c65ed6d
andy94077 <andy94077@gmail.com>
2020-03-09 16:21:49 +0800

    Add validation and hours feature in hw1.py

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -19,6 +19,8 @@ if __name__ == '__main__':
     parser.add_argument('-T', '--no-training', action='store_true')
     parser.add_argument('-s', '--test', nargs=2, help='testing file and the predicted file')
     parser.add_argument('--lr', type=float, default=1e-3, help='learning rate. (default: %(default)s)')
+    parser.add_argument('--hr', type=int, default=9, help='hours taken to predict. (default: %(default)s)')
+    parser.add_argument('-v', '--split-ratio', type=float, default=0.0, help='split ratio for validation set. (default: %(default)s)')
     args = parser.parse_args()
 
     model_path = args.model_path
@@ -26,12 +28,16 @@ if __name__ == '__main__':
     training = not args.no_training
     test = args.test
     lr = args.lr
+    hr = args.hr
+    split_ratio = args.split_ratio
 
     if training:
-        trainX, trainY, mean, std = utils.load_train_data(train_file, 9)
+        trainX, trainY, mean, std = utils.load_train_data(train_file, hr)
+        if split_ratio > 0:
+            trainX, validX, trainY, validY = utils.train_test_split(trainX, trainY, 0.1)
         print(f'\033[32;1mtrainX: {trainX.shape}, trainY: {trainY.shape}\033[0m')
-        np.save('mean.npy', mean)
-        np.save('std.npy', std)
+        np.save(model_path[:model_path.rfind('.npy')] + '_mean.npy', mean)
+        np.save(model_path[:model_path.rfind('.npy')] + '_std.npy', std)
 
         optimizer = Adam(compute_gradient, lr)
 
@@ -42,21 +48,28 @@ if __name__ == '__main__':
         for epoch in range(epochs):
             optimizer.update(XTX, XTY, w)
             if epoch % 100 == 0:
-                print(f'epoch {epoch + 100:04}, loss: {rmse(trainX, trainY, w):.5}')
-        a = w[1:].reshape(-1, 9)
+                if split_ratio > 0:
+                    print(f'epoch {epoch + 100:04}, loss: {rmse(trainX, trainY, w):.5}, valid_loss: {rmse(validX, validY, w):.5}')
+                else:
+                    print(f'epoch {epoch + 100:04}, loss: {rmse(trainX, trainY, w):.5}')
+        a = w[1:].reshape(-1, hr)
         for i in a:
-            print(('%.3f '*9) % tuple(i))
+            print(('%.3f '*hr) % tuple(i))
         np.save(model_path, w)
     else:
         w = np.load(model_path)
-        mean, std = np.load('mean.npy'), np.load('std.npy')
+        mean, std = np.load(model_path[:model_path.rfind('.npy')] + '_mean.npy'), np.load(model_path[:model_path.rfind('.npy')] + '_std.npy')
 
     if test:
         testX = utils.load_test_data(test[0], mean, std)
         utils.generate_csv(testX @ w, test[1])
     else:
         if not training:
-            trainX, trainY, mean, std = utils.load_train_data(train_file, 9)
+            trainX, trainY, mean, std = utils.load_train_data(train_file, hr)
+            if split_ratio > 0:
+                trainX, validX, trainY, validY = utils.train_test_split(trainX, trainY, 0.1)
         print(f'Training loss: {rmse(trainX, trainY, w)}')
+        if split_ratio > 0:
+            print(f'Validation loss: {rmse(validX, validY, w)}')
 
 

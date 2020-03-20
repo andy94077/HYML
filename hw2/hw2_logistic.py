254d9ccb5b84a35de3888b8b6ce91a8d43782397
andy94077 <andy94077@gmail.com>
2020-03-20 12:16:53 +0800

    Add preprocessing option and ensemble feature in hw2_logistic.py

--- hw2/hw2_logistic.py
+++ hw2/hw2_logistic.py
@@ -16,7 +16,7 @@ def loss(X, Y, w):
     return -np.mean(Y * np.log(y_pred + 1e-10) + (1 - Y) * np.log(1 - y_pred + 1e-10))
 
 def gradient(X, Y, w):
-    return X.T @ (f(X, w) - Y) / X.shape[0]# + 1e-4 * 2 * w ** 2
+    return X.T @ (f(X, w) - Y) / X.shape[0]
 
 def accuracy(X, Y, w):
     return np.mean((np.sign(X @ w).ravel() + 1) / 2 == Y.ravel())
@@ -27,14 +27,16 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--training-file', nargs=2, help='trainX and trainY')
     parser.add_argument('-T', '--no-training', action='store_true')
     parser.add_argument('-s', '--test', nargs=2, help='testing file and the predicted file')
+    parser.add_argument('-p', '--preprocessing', action='store_true', help='preprocess testing file and the predicted file')
     args = parser.parse_args()
 
     model_path = args.model_path
     train_file = args.training_file
     training = not args.no_training
     test = args.test
+    preprocessing = args.preprocessing
 
-    trainX, trainY, mean, std = utils.load_train_data(train_file[0], train_file[1], preprocessing=True)
+    trainX, trainY, mean, std = utils.load_train_data(train_file[0], train_file[1], preprocessing=preprocessing)
     trainX, validX, trainY, validY = utils.train_test_split(trainX, trainY)
     print(f'\033[32;1mtrainX: {trainX.shape}, trainY: {trainY.shape}, validX: {validX.shape}, validY: {validY.shape}\033[0m')
     if training:
@@ -55,8 +57,9 @@ if __name__ == '__main__':
         w = np.load(model_path)
 
     if test:
-        testX = utils.load_test_data(test[0], mean, std, preprocessing=True)
+        testX = utils.load_test_data(test[0], mean, std, preprocessing=preprocessing)
         utils.generate_csv(f(testX, w), test[1])
+        np.save(test[1] + '.npy', f(testX, w))
     else:
         print(f'loss: {loss(trainX, trainY, w):.5}, acc: {accuracy(trainX, trainY, w):.4}, valid_loss: {loss(validX, validY, w):.5}, valid_acc: {accuracy(validX, validY, w):.4}')
 

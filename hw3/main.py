4509e20df8d4be97e26ffc78cd7b07afc72d9893
andy94077 <andy94077@gmail.com>
2020-04-01 14:19:54 +0800

    Modify build_model2 in main.py

--- hw3/main.py
+++ hw3/main.py
@@ -1,8 +1,9 @@
 import os, sys, argparse
+import importlib
 import numpy as np
 from keras.models import Model, Sequential
-from keras.layers import Input, Dense, BatchNormalization, Dropout, Conv2D, MaxPooling2D, Flatten, Activation
-from keras.optimizers import Adam
+from keras.layers import Input, Dense, BatchNormalization, Dropout, Conv2D, MaxPooling2D, Flatten, Activation, GlobalAveragePooling2D
+from keras.optimizers import Adam, SGD
 from keras.regularizers import l2
 from keras.utils import plot_model
 from keras.preprocessing.image import ImageDataGenerator
@@ -41,38 +42,38 @@ def build_model(input_shape, output_dim):
         ])
     return model
 
-def build_model2(input_shape, output_dim): #{{{
+def build_model2(input_shape, output_dim):
     model = Sequential([
         Conv2D(64, 3, padding='same', activation='relu', input_shape=input_shape),
+        BatchNormalization(),
         Conv2D(64, 3, padding='same', activation='relu'),
         BatchNormalization(),
         Conv2D(64, 3, strides=2, padding='same', activation='relu'),
         BatchNormalization(),
 
-        Conv2D(128, 3, padding='same', activation='relu'),
-        Conv2D(128, 3, padding='same', activation='relu'),
-        BatchNormalization(),
         Conv2D(128, 3, strides=2, padding='same', activation='relu'),
         BatchNormalization(),
+        Conv2D(128, 3, padding='same', activation='relu'),
+        BatchNormalization(),
+        Conv2D(128, 3, padding='same', activation='relu'),
+        BatchNormalization(),
 
-        Conv2D(256, 3, padding='same', activation='relu'),
         Conv2D(256, 3, strides=2, padding='same', activation='relu'),
         BatchNormalization(),
-
-        Conv2D(512, 3, strides=2, padding='same', activation='relu'),
+        Conv2D(256, 3, padding='same', activation='relu'),
         BatchNormalization(),
 
         Conv2D(512, 3, strides=2, padding='same', activation='relu'),
         BatchNormalization(),
 
-        Flatten(),
+        Conv2D(512, 3, strides=2, padding='same', activation='relu'),
+        BatchNormalization(),
+
+        GlobalAveragePooling2D(),
         Dense(1024, activation='relu'),
-        Dropout(0.3),
-        Dense(1024, activation='relu'),
-        Dropout(0.3),
         Dense(output_dim, activation='softmax')
         ])
-    return model #}}}
+    return model
 
 if __name__ == '__main__':
     os.environ['CUDA_VISIBLE_DEVICES'] = '3' 
@@ -89,6 +90,7 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--test', type=str, help='predicted file')
     parser.add_argument('-r', '--seed', type=int, default=880301, help='random seed')
     parser.add_argument('-e', '--ensemble', action='store_true', help='output npy file to ensemble later')
+    parser.add_argument('-lr', type=float, default=1.5e-3, help='learning rate')
     args = parser.parse_args()
 
     data_dir = args.data_dir
@@ -98,22 +100,25 @@ if __name__ == '__main__':
     ensemble = args.ensemble
     function = args.model_function
     seed = args.seed
+    lr = args.lr
     input_shape = (128, 128)
 
     if training:
-        trainX, trainY = utils.load_train_data(data_dir, input_shape)
+        trainX, trainY = utils.load_train_data(data_dir, input_shape, preprocessing=True)
         trainX, validX, trainY, validY = utils.train_test_split(trainX, trainY, split_ratio=0.1, seed=seed)
         print(f'\033[32;1mtrainX: {trainX.shape}, trainY: {trainY.shape}, validX: {validX.shape}, validY: {validY.shape}\033[0m')
         train_gen = ImageDataGenerator(rotation_range=15, width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True, fill_mode='constant', cval=0)
 
-    model = build_model(input_shape + (3,), 11)
-    model.compile(Adam(1.5e-3), loss='categorical_crossentropy', metrics=['acc'])
+    if function not in globals():
+        globals()[function] = getattr(importlib.import_module(function[:function.rfind('.')]), function.split('.')[-1])
+    model = globals()[function](input_shape + (3,), 11)
+    model.compile(Adam(lr), loss='categorical_crossentropy', metrics=['acc'])
     model.summary()
 
     if training:
         batch_size = 128
-        checkpoint = ModelCheckpoint(model_path, 'val_loss', verbose=1, save_best_only=True, save_weights_only=True)
-        reduce_lr = ReduceLROnPlateau('val_loss', 0.8, 5, verbose=1, min_lr=1e-4)
+        checkpoint = ModelCheckpoint(model_path, 'val_acc', verbose=1, save_best_only=True, save_weights_only=True)
+        reduce_lr = ReduceLROnPlateau('val_acc', 0.8, 5, verbose=1, min_lr=1e-4)
         #logger = CSVLogger(model_path+'.csv')
         #tensorboard = TensorBoard(model_path[:model_path.rfind('.')]+'_logs', histogram_freq=1, batch_size=1024, write_grads=True, update_freq='epoch')
         model.fit_generator(train_gen.flow(trainX, trainY, batch_size=batch_size), steps_per_epoch=trainX.shape[0]//batch_size, epochs=50, validation_data=(validX, validY), verbose=1, callbacks=[checkpoint, reduce_lr])
@@ -122,7 +127,7 @@ if __name__ == '__main__':
 
     model.load_weights(model_path)
     if test:
-        testX = utils.load_test_data(data_dir, input_shape)
+        testX = utils.load_test_data(data_dir, input_shape, preprocessing=True)
         pred = model.predict(testX)
         if ensemble:
             np.save(test, pred)
@@ -130,9 +135,9 @@ if __name__ == '__main__':
             utils.generate_csv(pred, test)
     else:
         if not training:
-            trainX, trainY = utils.load_train_data(data_dir, input_shape)
+            trainX, trainY = utils.load_train_data(data_dir, input_shape, preprocessing=True)
             trainX, validX, trainY, validY = utils.train_test_split(trainX, trainY, split_ratio=0.1, seed=seed)
             print(f'\033[32;1mtrainX: {trainX.shape}, trainY: {trainY.shape}, validX: {validX.shape}, validY: {validY.shape}\033[0m')
-        print(f'\033[32;1mTraining score: {model.evaluate(trainX, trainY, batch_size=256, verbose=0)}\033[0m')
-        print(f'\033[32;1mValidaiton score: {model.evaluate(validX, validY, batch_size=256, verbose=0)}\033[0m')
+        print(f'\033[32;1mTraining score: {model.evaluate(trainX, trainY, batch_size=128, verbose=0)}\033[0m')
+        print(f'\033[32;1mValidaiton score: {model.evaluate(validX, validY, batch_size=128, verbose=0)}\033[0m')
         

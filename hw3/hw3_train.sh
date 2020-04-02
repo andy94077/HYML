66dbd88226e59290b895f85aa55ccf2fef949902
andy94077 <andy94077@gmail.com>
2020-04-02 22:38:05 +0800

    Modify hw3_train.sh and hw3_test.sh

--- hw3/hw3_train.sh
+++ hw3/hw3_train.sh
@@ -6,4 +6,5 @@ cd "$cur_dir"
 python3 main.py "$data_dir" model/test.h5 -r 880301
 python3 main.py "$data_dir" model/test2.h5 -r 1126
 python3 main.py "$data_dir" model/model2.h5 -f build_model2 -lr 1e-3
+python3 main.py "$data_dir" model/model2_no_norm.h5 -N -f build_model2 -lr 1e-3
 

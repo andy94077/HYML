21aa0cc0fdccc63a7418c123ac260da1e17cbcfc
andy94077 <andy94077@gmail.com>
2020-04-17 12:07:16 +0800

    Modify hw3_train.sh, hw3_test.sh

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -1,10 +1,18 @@
 testX="$1"
 predicted_file="$2"
-gpu=0
+gpu=3
 python3 main.py model/word2vec model/lstm128.h5 -g $gpu -f build_model2 -eTs "$testX" "$predicted_file.1.npy"
 python3 main.py model/word2vec model/lstm128-3.h5 -g $gpu -eTs "$testX" "$predicted_file.2.npy"
 python3 main_acc.py model/word2vec model/lstm128-3_acc.h5 -g $gpu -eTs "$testX" "$predicted_file.3.npy"
 python3 main.py model/word2vec model/lstm128_semi.h5 -g $gpu -eTs "$testX" "$predicted_file.4.npy"
-python3 ensemble.py "$predicted_file" "$predicted_file.1.npy" "$predicted_file.2.npy" "$predicted_file.3.npy" "$predicted_file.4.npy"
-rm -f "$predicted_file.1.npy" "$predicted_file.2.npy" "$predicted_file.3.npy" "$predicted_file.4.npy"
+python3 main.py model/word2vec300_iter_10 model/w2v_300_iter_10_lstm128.h5 -g $gpu -f build_model2 -eTs "$testX" "$predicted_file.5.npy"
+python3 main.py model/word2vec300_iter_10 model/w2v_300_iter_10_lstm128-3.h5 -g $gpu -eTs "$testX" "$predicted_file.6.npy"
+python3 main_acc.py model/word2vec300_iter_10 model/w2v_300_iter_10_lstm128-3_acc.h5 -g $gpu -eTs "$testX" "$predicted_file.7.npy"
+python3 main.py model/word2vec300_iter_10 model/w2v_300_iter_10_lstm128_semi.h5 -g $gpu  -eTs "$testX" "$predicted_file.8.npy"
+python3 main.py model/word2vec300 model/w2v_300_lstm128.h5 -g $gpu -f build_model2 -eTs "$testX" "$predicted_file.9.npy"
+python3 main.py model/word2vec300 model/w2v_300_lstm128-3.h5 -g $gpu -eTs "$testX" "$predicted_file.10.npy"
+python3 main_acc.py model/word2vec300 model/w2v_300_lstm128-3_acc.h5 -g $gpu -eTs "$testX" "$predicted_file.11.npy"
+python3 main.py model/word2vec300 model/w2v_300_lstm128_semi.h5 -g $gpu -eTs "$testX" "$predicted_file.12.npy"
+python3 ensemble.py "$predicted_file" $(for i in {1..12}; do echo -n "$predicted_file.$i.npy "; done) #"$predicted_file.1.npy" "$predicted_file.2.npy" "$predicted_file.3.npy" "$predicted_file.4.npy"
+rm -f $(for i in {1..12}; do echo -n "$predicted_file.$i.npy "; done) #"$predicted_file.1.npy" "$predicted_file.2.npy" "$predicted_file.3.npy" "$predicted_file.4.npy"
 
--- hw4/hw4_train.sh
+++ hw4/hw4_train.sh
@@ -1,5 +1,15 @@
 trainX="$1"
 trainX_no_label="$2"
-python3 main.py model/word2vec model/lstm128.h5 -g 0 -f build_model2 -t "$trainX" "$trainX_no_label"
-python3 main.py model/word2vec model/lstm128-3.h5 -g 0 -t "$trainX" "$trainX_no_label"
-python3 main_acc.py model/word2vec model/lstm128-3_acc.h5 -g 0 -t "$trainX" "$trainX_no_label"
+gpu=3
+python3 main.py model/word2vec model/lstm128.h5 -g $gpu -f build_model2 -t "$trainX" "$trainX_no_label"
+python3 main.py model/word2vec model/lstm128-3.h5 -g $gpu -t "$trainX" "$trainX_no_label"
+python3 main_acc.py model/word2vec model/lstm128-3_acc.h5 -g $gpu -t "$trainX" "$trainX_no_label"
+python3 main.py model/word2vec model/lstm128_semi.h5 -g $gpu -t "$trainX" "$trainX_no_label" --semi
+python3 main.py model/word2vec300_iter_10 model/w2v_300_iter_10_lstm128.h5 -g $gpu -f build_model2 -t "$trainX" "$trainX_no_label"
+python3 main.py model/word2vec300_iter_10 model/w2v_300_iter_10_lstm128-3.h5 -g $gpu -t "$trainX" "$trainX_no_label"
+python3 main_acc.py model/word2vec300_iter_10 model/w2v_300_iter_10_lstm128-3_acc.h5 -g $gpu -t "$trainX" "$trainX_no_label"
+python3 main.py model/word2vec300_iter_10 model/w2v_300_iter_10_lstm128_semi.h5 -g $gpu -t "$trainX" "$trainX_no_label" --semi
+python3 main.py model/word2vec300 model/w2v_300_lstm128.h5 -g $gpu -f build_model2 -t "$trainX" "$trainX_no_label"
+python3 main.py model/word2vec300 model/w2v_300_lstm128-3.h5 -g $gpu -t "$trainX" "$trainX_no_label"
+python3 main_acc.py model/word2vec300 model/w2v_300_lstm128-3_acc.h5 -g $gpu -t "$trainX" "$trainX_no_label"
+python3 main.py model/word2vec300 model/w2v_300_lstm128_semi.h5 -g $gpu -t "$trainX" "$trainX_no_label" --semi

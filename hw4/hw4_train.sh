4d83f80b311321e69f8e1e4e4bef9170bf3bb7ff
andy94077 <andy94077@gmail.com>
2020-04-17 12:24:17 +0800

    Update hw4_train.sh, hw4_test.sh

--- hw4/hw4_train.sh
+++ hw4/hw4_train.sh
@@ -1,6 +1,6 @@
 trainX="$1"
 trainX_no_label="$2"
-gpu=3
+gpu=0
 python3 main.py model/word2vec model/lstm128.h5 -g $gpu -f build_model2 -t "$trainX" "$trainX_no_label"
 python3 main.py model/word2vec model/lstm128-3.h5 -g $gpu -t "$trainX" "$trainX_no_label"
 python3 main_acc.py model/word2vec model/lstm128-3_acc.h5 -g $gpu -t "$trainX" "$trainX_no_label"

f514713eb87269e2afd7a90fe19d8349170f8c5c
andy94077 <andy94077@gmail.com>
2020-04-17 12:05:47 +0800

    Add preprocessing parameter in utils.py

--- hw4/utils.py
+++ hw4/utils.py
@@ -14,25 +14,27 @@ def data_preprocessing(X, word2idx, max_seq_len):
     X = [[word2idx.get(w, word2idx['<UNK>']) for w in text] for text in X]
     return pad_sequences(X, max_seq_len, padding='post', truncating='post', value=word2idx['<PAD>'])
 
-def load_train_data(path, word2idx, max_seq_len, label=True):
+def load_train_data(path, word2idx=None, max_seq_len=None, label=True, preprocessing=True):
     if label:
         with open(path, 'r') as f:
             f_all = [ll.split('+++$+++') for ll in f.readlines()]
         trainX = [text_to_word_sequence(ll[1]) for ll in f_all]  # split the string to list of words
-        trainX = data_preprocessing(trainX, word2idx, max_seq_len)
+        if preprocessing:
+            trainX = data_preprocessing(trainX, word2idx, max_seq_len)
         trainY = np.array([int(ll[0]) for ll in f_all])
         return trainX, trainY
     else:
         with open(path, 'r') as f:
             trainX = [text_to_word_sequence(ll) for ll in f.readlines()]  # split the string to list of words
-        trainX = data_preprocessing(trainX, word2idx, max_seq_len)
+        if preprocessing:
+            trainX = data_preprocessing(trainX, word2idx, max_seq_len)
         return trainX
         
-def load_test_data(path, word2idx, max_seq_len):
+def load_test_data(path, word2idx=None, max_seq_len=None, preprocessing=True):
     with open(path, 'r') as f:
         f_all = [ll[ll.find(',') + 1:] for ll in f.readlines()[1:]]
     test = [text_to_word_sequence(ll) for ll in f_all]
-    return data_preprocessing(test, word2idx, max_seq_len)  # split the string to list of words
+    return data_preprocessing(test, word2idx, max_seq_len) if preprocessing else test  # split the string to list of words
 
 def train_test_split(X, Y, split_ratio=0.2, seed=880301):
     np.random.seed(seed)

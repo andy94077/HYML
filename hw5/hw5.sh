624cff9897f28c3c10e506519fdb3dbb5f8d3738
andy94077 <andy94077@gmail.com>
2020-04-26 14:26:10 +0800

    Update hw5.sh

--- hw5/hw5.sh
+++ hw5/hw5.sh
@@ -3,3 +3,4 @@ output_dir="$2"
 
 if [ ! -d "$output_dir" ]; then mkdir "$output_dir"; fi
 python3 main.py "$data_dir" model/model2_no_norm.h5 -g 0 -o "$output_dir"
+python3 confusion_matrix.py model/model2_no_norm-2.h5 -T -g 0 -o "$output_dir"

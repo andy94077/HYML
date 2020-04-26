79d8aec9267f395e0be99da5b57afa9345192b9f
andy94077 <andy94077@gmail.com>
2020-04-26 12:17:35 +0800

    Resize image while opening it in dataset.py

--- hw6/dataset.py
+++ hw6/dataset.py
@@ -10,7 +10,7 @@ import torchvision.transforms as transforms
 class Adverdataset(Dataset):
 	def __init__(self, data_dir, transforms):
 		df = pd.read_csv(os.path.join(data_dir, 'labels.csv'))
-		self.X = [Image.open(os.path.join(data_dir, 'images', f'{name:0{len(str(df.shape[0]))}}.png')) for name in df['ImgId']]
+		self.X = [Image.open(os.path.join(data_dir, 'images', f'{name:0{len(str(df.shape[0]))}}.png')).resize((224, 224)) for name in df['ImgId']]
 		self.label = torch.from_numpy(df['TrueLabel'].to_numpy()).long()
 		self.transforms = transforms
 

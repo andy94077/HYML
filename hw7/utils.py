934a75b4eb65f840f4f8f50a6588188ca31d79c8
andy94077 <andy94077@gmail.com>
2020-05-09 20:17:24 +0800

    Fix minor bugs in utils.py

--- hw7/utils.py
+++ hw7/utils.py
@@ -16,7 +16,7 @@ def load_train_filename_and_labels(data_dir):
     '''
     val_filename = os.listdir(os.path.join(data_dir, 'validation'))
     train_filename = sorted(os.listdir(os.path.join(data_dir, 'training')) + [name[:-3] + '_.jpg' for name in val_filename])
-    filenames = [os.path.join(data_dir, 'training', n) if n[-5] != '_' else os.path.join(data_dir, 'validation', n[:-5] + 'jpg') for n in tqdm(train_filename)]
+    filenames = [os.path.join(data_dir, 'training', n) if n[-5] != '_' else os.path.join(data_dir, 'validation', n[:-5] + 'jpg') for n in train_filename]
     labels = [int(os.path.basename(name).split('_')[0]) for name in filenames]
     return filenames, labels
 
@@ -30,12 +30,13 @@ def load_test_filename(data_dir):
     filename = [os.path.join(data_dir, 'testing', name) for name in os.listdir(os.path.join(data_dir, 'testing'))]
     return filename
 
-def train_test_split(X, Y=None, split_ratio=0.2, seed=880301):
+def train_test_split(X, Y, split_ratio=0.2, seed=880301):
     np.random.seed(seed)
-    split_idx = [np.random.permutation(np.arange(Y.shape[0])[Y[:, i] == 1]) for i in range(Y.shape[1])]
+    Y = np.array(Y)
+    split_idx = [np.random.permutation(np.arange(Y.shape[0])[Y == i]) for i in range(np.max(Y))]
     train_idx = np.concatenate([idx[:-int(idx.shape[0]*split_ratio)] for idx in split_idx], axis=0)
     valid_idx = np.concatenate([idx[-int(idx.shape[0]*split_ratio):] for idx in split_idx], axis=0)
-    return X[train_idx], X[valid_idx], Y[train_idx], Y[valid_idx]
+    return [X[i] for i in train_idx], [X[i] for i in valid_idx], Y[train_idx], Y[valid_idx]
 
 def generate_csv(y_pred, output_file):
     Y = np.argmax(y_pred, axis=1).astype(np.int)

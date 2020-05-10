a53f4c9b19f527aaed682898b0ed836fa546e270
andy94077 <andy94077@gmail.com>
2020-05-11 00:57:26 +0800

    Init clustering.py

---
+++ hw9/clustering.py
@@ -0,0 +1,23 @@
+from sklearn.cluster import KMeans
+from sklearn.decomposition import KernelPCA
+from MulticoreTSNE import MulticoreTSNE as TSNE
+
+class Clustering():
+	def __init__(self, dimension_reduction=False):
+		self.dimension_reduction = dimension_reduction
+		if dimension_reduction:
+			self.pca = KernelPCA(200, 'rbf', n_jobs=-1, random_state=880301)
+		self.kmeans = KMeans(n_clusters=2, n_jobs=-1, random_state=880301)
+	
+	def fit(self, X):
+		if self.dimension_reduction:
+			X = self.pca.fit_transform(X)
+			X = TSNE(n_components=2, n_jobs=-1, random_state=880301).fit_transform(X)
+		self.kmeans.fit(X)
+		return self
+	
+	def predict(self, X):
+		if self.dimension_reduction:
+			X = self.pca.transform(X)
+			X = TSNE(n_components=2, n_jobs=-1, random_state=880301).fit_transform(X)
+		return self.kmeans.predict(X)

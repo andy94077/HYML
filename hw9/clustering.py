d4319da4eac69118569895e47721b871b6a8ac5b
andy94077 <andy94077@gmail.com>
2020-05-11 22:07:50 +0800

    Modify Clustering class in clustering.py

--- hw9/clustering.py
+++ hw9/clustering.py
@@ -4,19 +4,33 @@ from sklearn.manifold import TSNE
 
 class Clustering():
 	def __init__(self):
-		self.pca = KernelPCA(1024, 'rbf', n_jobs=16)
-		self.pca2 = PCA(64)
-		self.kmeans = KMeans(n_clusters=2, n_jobs=-1)
+		self.seed = int(1e9+7)
+		self.pca = KernelPCA(1024, 'rbf', n_jobs=-1, random_state=self.seed)
+		self.pca2 = KernelPCA(64, 'rbf', n_jobs=-1, random_state=self.seed)
+		self.kmeans = KMeans(n_clusters=2, n_jobs=-1, random_state=self.seed)
 	
 	def fit(self, X):
+		print('fitting pca...')
 		X = self.pca.fit_transform(X)
+		print('fitting pca2...')
 		X = self.pca2.fit_transform(X)
-		X = TSNE(n_components=2, n_jobs=32, random_state=880531).fit_transform(X)
+		print('fitting TSNE...')
+		X = TSNE(n_components=2, n_jobs=-1, random_state=self.seed).fit_transform(X)
 		self.kmeans.fit(X)
 		return self
 	
+	def fit_predict(self, X):
+		print('fitting pca...')
+		X = self.pca.fit_transform(X)
+		print('fitting pca2...')
+		X = self.pca2.fit_transform(X)
+		print('fitting TSNE...')
+		X = TSNE(n_components=2, n_jobs=-1, random_state=self.seed).fit_transform(X)
+		out = self.kmeans.fit_predict(X)
+		return self, out
+
 	def predict(self, X):
 		X = self.pca.transform(X)
 		X = self.pca2.transform(X)
-		X = TSNE(n_components=2, n_jobs=8, random_state=880531).fit_transform(X)
+		X = TSNE(n_components=2, n_jobs=-1, random_state=self.seed).fit_transform(X)
 		return self.kmeans.predict(X)

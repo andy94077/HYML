204e5b9c3994204723e3bd6ae5fd129fd34ab69d
andy94077 <andy94077@gmail.com>
2020-05-13 16:05:54 +0800

    Add build_autoencoder3, build_autoencoder4 in autoencoder.py

--- hw9/autoencoder.py
+++ hw9/autoencoder.py
@@ -2,7 +2,7 @@ import os, argparse
 import importlib
 import numpy as np
 from keras.models import Model, Sequential
-from keras.layers import Input, Dense, BatchNormalization, Dropout, Conv2D, MaxPooling2D, Flatten, Activation, Conv2DTranspose
+from keras.layers import Input, Dense, BatchNormalization, Dropout, Conv2D, MaxPooling2D, Flatten, Activation, Conv2DTranspose, GlobalAveragePooling2D
 from keras.optimizers import Adam, SGD
 from keras.regularizers import l2
 from keras.utils import plot_model
@@ -93,13 +93,86 @@ def build_autoencoder2(input_shape):
     ])
     return encoder, decoder, autoencoder
 
+def build_autoencoder3(input_shape):
+    '''
+    Arguments:
+        input_shape(tuple): the shape of input images (H, W, C)
+    
+    Returns:
+        encoder, decoder, autoencoder models
+    '''
+    def build_encoder(input_shape):
+        encoder = Sequential([
+            Conv2D(64, 3, padding='same', activation='relu', input_shape=input_shape),
+            MaxPooling2D(),
+
+            Conv2D(128, 3, padding='same', activation='relu'),
+            MaxPooling2D(),
+            
+            Conv2D(256, 3, padding='same', activation='relu'),
+            MaxPooling2D(),
+
+            Conv2D(256, 3, padding='same', activation='relu'),
+            MaxPooling2D()
+        ])
+        return encoder
+
+    def build_decoder(hidden_shape):
+        decoder = Sequential([
+            Conv2DTranspose(256, 3, strides=2, padding='same', activation='relu', input_shape=hidden_shape),
+            Conv2DTranspose(128, 3, strides=2, padding='same', activation='relu'),
+            Conv2DTranspose(64, 3, strides=2, padding='same', activation='relu'),
+            Conv2DTranspose(3, 3, strides=2, padding='same', activation='tanh')
+        ])
+        return decoder
+
+    encoder = build_encoder(input_shape)
+    decoder = build_decoder(encoder.layers[-1].output_shape[-3:])
+    autoencoder = Sequential([
+        encoder,
+        decoder
+    ])
+    return encoder, decoder, autoencoder
+
+def build_autoencoder4(input_shape):
+    '''
+    Arguments:
+        input_shape(tuple): the shape of input images (H, W, C)
+    
+    Returns:
+        encoder, decoder, autoencoder models
+    '''
+    def build_encoder(input_shape):
+        encoder = Sequential([
+            Conv2D(64, 3, strides=2, padding='same', activation='relu', input_shape=input_shape),
+            Conv2D(128, 3, strides=2, padding='same', activation='relu'),
+            Conv2D(256, 3, strides=2, padding='same', activation='relu'),
+            Conv2D(128, 3, strides=2, padding='same', activation='relu'),
+        ])
+        return encoder
+
+    def build_decoder(hidden_shape):
+        decoder = Sequential([
+            Conv2DTranspose(256, 3, strides=2, padding='same', activation='relu', input_shape=hidden_shape),
+            Conv2DTranspose(128, 3, strides=2, padding='same', activation='relu'),
+            Conv2DTranspose(64, 3, strides=2, padding='same', activation='relu'),
+            Conv2DTranspose(3, 3, strides=2, padding='same', activation='tanh')
+        ])
+        return decoder
+
+    encoder = build_encoder(input_shape)
+    decoder = build_decoder(encoder.layers[-1].output_shape[-3:])
+    autoencoder = Sequential([
+        encoder,
+        decoder
+    ])
+    return encoder, decoder, autoencoder
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('model_path')
     parser.add_argument('-t', '--training-file', help='training data')
     parser.add_argument('-v', '--validation-file', help='validation data')
-    parser.add_argument('-y', '--validation-labels', help='validation label')
     parser.add_argument('-f', '--model-function', default='build_autoencoder')
     parser.add_argument('-T', '--no-training', action='store_true')
     parser.add_argument('-s', '--test', nargs=2, type=str, help='testing file and predicted file')
@@ -115,7 +188,6 @@ if __name__ == '__main__':
     model_path = args.model_path
     trainX_path = args.training_file
     validX_path = args.validation_file
-    validY_path = args.validation_labels
     function = args.model_function
     training = not args.no_training
     test = args.test
@@ -142,7 +214,7 @@ if __name__ == '__main__':
         #logger = CSVLogger(model_path+'.csv')
         #tensorboard = TensorBoard(model_path[:model_path.rfind('.')]+'_logs', histogram_freq=1, batch_size=1024, write_grads=True, update_freq='epoch')
         # model.fit_generator(train_gen.flow(trainX, trainX, batch_size=batch_size), steps_per_epoch=trainX.shape[0]/batch_size, epochs=50, validation_data=(validX, validX), verbose=1, callbacks=[checkpoint, reduce_lr])
-        model.fit(trainX, trainX, batch_size=batch_size, epochs=50, validation_data=(validX, validX), verbose=1, callbacks=[checkpoint, reduce_lr])
+        model.fit(trainX, trainX, batch_size=batch_size, epochs=100, validation_data=(validX, validX), verbose=1, callbacks=[checkpoint, reduce_lr])
     else:
         print('\033[32;1mLoading Model\033[0m')
 
@@ -158,9 +230,4 @@ if __name__ == '__main__':
             print(f'\033[32;1mtrainX: {trainX.shape}, validX: {validX.shape}\033[0m')
         print(f'\033[32;1mTraining loss: {model.evaluate(trainX, trainX, batch_size=256, verbose=0)}\033[0m')
         print(f'\033[32;1mValidaiton loss: {model.evaluate(validX, validX, batch_size=256, verbose=0)}\033[0m')
-        if validY_path:
-            validY = np.load(validY_path)
-            latentX = encoder.predict(validX, batch_size=256).reshape(validX.shape[0], -1)
-            clf = LinearSVC(max_iter=2000).fit(latentX, validY)
-            print(f'\033[32;1mValidaiton acc: {clf.score(latentX, validY)}\033[0m')
 

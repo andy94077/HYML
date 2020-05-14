208f7ff6df59e06a85124f2bc2b9ba603ad2fd8d
andy94077 <andy94077@gmail.com>
2020-05-14 14:06:45 +0800

    Remove validation in autoencoder.py

--- hw9/autoencoder.py
+++ hw9/autoencoder.py
@@ -10,8 +10,6 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, CSVLogger, TensorBoard
 from keras import backend as K
 import tensorflow as tf
-from sklearn.model_selection import train_test_split
-from sklearn.svm import LinearSVC
 
 import utils
 
@@ -172,10 +170,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('model_path')
     parser.add_argument('-t', '--training-file', help='training data')
-    parser.add_argument('-v', '--validation-file', help='validation data')
     parser.add_argument('-f', '--model-function', default='build_autoencoder')
     parser.add_argument('-T', '--no-training', action='store_true')
     parser.add_argument('-s', '--test', nargs=2, type=str, help='testing file and predicted file')
+    parser.add_argument('-e', '--epoch', type=int, default=50)
     parser.add_argument('-g', '--gpu', default='3', help='available gpu device')
     args = parser.parse_args()
 
@@ -187,7 +185,6 @@ if __name__ == '__main__':
 
     model_path = args.model_path
     trainX_path = args.training_file
-    validX_path = args.validation_file
     function = args.model_function
     training = not args.no_training
     test = args.test
@@ -200,34 +197,26 @@ if __name__ == '__main__':
     model.summary()
 
     if training:
-        trainX = utils.load_data(trainX_path, normalize=True, preprocessing=False)
-        if validX_path is not None:
-            validX = utils.load_data(validX_path, normalize=True, preprocessing=False)
-        else:
-            trainX, validX = train_test_split(trainX, test_size=0.1, random_state=880301)
-        print(f'\033[32;1mtrainX: {trainX.shape}, validX: {validX.shape}\033[0m')
-        # train_gen = ImageDataGenerator(rotation_range=15, width_shift_range=0.1, height_shift_range=0.1, horizontal_flip=True, zoom_range=0.1)
+        trainX = utils.load_data(trainX_path, normalize=True)
+        print(f'\033[32;1mtrainX: {trainX.shape}\033[0m')
 
         batch_size = 256
         checkpoint = ModelCheckpoint(model_path, 'loss', verbose=1, save_best_only=True, save_weights_only=True)
         reduce_lr = ReduceLROnPlateau('loss', 0.8, 5, verbose=1, min_lr=1e-5)
         #logger = CSVLogger(model_path+'.csv')
         #tensorboard = TensorBoard(model_path[:model_path.rfind('.')]+'_logs', histogram_freq=1, batch_size=1024, write_grads=True, update_freq='epoch')
-        # model.fit_generator(train_gen.flow(trainX, trainX, batch_size=batch_size), steps_per_epoch=trainX.shape[0]/batch_size, epochs=50, validation_data=(validX, validX), verbose=1, callbacks=[checkpoint, reduce_lr])
-        model.fit(trainX, trainX, batch_size=batch_size, epochs=100, validation_data=(validX, validX), verbose=1, callbacks=[checkpoint, reduce_lr])
+        model.fit(trainX, trainX, batch_size=batch_size, epochs=args.epoch, verbose=1, callbacks=[checkpoint, reduce_lr])
     else:
         print('\033[32;1mLoading Model\033[0m')
 
     model.load_weights(model_path)
     if test:
-        testX = utils.load_data(test[0], normalize=True, preprocessing=False)
+        testX = utils.load_data(test[0], normalize=True)
         pred = encoder.predict(testX, batch_size=256)
         np.save(test[1], pred)
     else:
         if not training:
-            trainX = utils.load_data(trainX_path, normalize=True, preprocessing=False)
-            validX = utils.load_data(validX_path, normalize=True, preprocessing=False)
-            print(f'\033[32;1mtrainX: {trainX.shape}, validX: {validX.shape}\033[0m')
+            trainX = utils.load_data(trainX_path, normalize=True)
+            print(f'\033[32;1mtrainX: {trainX.shape}\033[0m')
         print(f'\033[32;1mTraining loss: {model.evaluate(trainX, trainX, batch_size=256, verbose=0)}\033[0m')
-        print(f'\033[32;1mValidaiton loss: {model.evaluate(validX, validX, batch_size=256, verbose=0)}\033[0m')
 
